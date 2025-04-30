6be77a47ea40f72cf112da6f16b72ca8098f5a25
hermangavrylov <gavrylovherman@gmail.com>
2025-04-30 02:36:43 +0200

    Add functionality for "Search"

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
      QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, \
      QDialog, QVBoxLayout, QComboBox
@@ -103,9 +104,9 @@ class SearchDialog(QDialog):
 
         layout = QVBoxLayout()
 
-        student_name = QLineEdit()
-        student_name.setPlaceholderText("Name")
-        layout.addWidget(student_name)
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
 
         button = QPushButton("Search")
         button.clicked.connect(self.search_student)
@@ -114,7 +115,18 @@ class SearchDialog(QDialog):
         self.setLayout(layout)
 
     def search_student(self):
-        pass
+        name = self.student_name.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
+        rows = list(result)
+        items = main_window.table.findItems(name, Qt.MatchFlag.MatchFixedString)
+        for item in items:
+            print(item)
+            main_window.table.item(item.row(), 1).setSelected(True)
+
+        cursor.close()
+        connection.close()
 
 
 app = QApplication(sys.argv)
